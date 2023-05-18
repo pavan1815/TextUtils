2f15b477f13ac70f2adc2215b3ac0168a1701aa3
pavan1815 <pavanreddyvancha@gmail.com>
2023-05-18 12:12:29 +0530

    Updates

---
+++ precache-manifest.904e4765ebc989a757adbe636eb9b9c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07690c8c9f9c1c4c978",
+    "url": "/TextUtils/static/css/main.d96ff8d7.chunk.css"
+  },
+  {
+    "revision": "f07690c8c9f9c1c4c978",
+    "url": "/TextUtils/static/js/main.f07690c8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "83ad030430eed9ad6b9a8f4bff36f3c3",
+    "url": "/TextUtils/index.html"
+  }
+];
